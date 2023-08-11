2b478b2cfd3d84a60eba3806476e32a59fb9d73b
zhang <13811168949@139.com>
2023-08-11 13:20:32 +0800

    anything

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -14,13 +14,10 @@
 use std::f32;
 
 fn main() {
-    let pi = 3.14f32;
-    let radius = 5.00f32;
+    let pi: f32 = 3.14;
+    let radius: f32 = 5.0;
 
-    let area = pi * f32::powi(radius, 2);
+    let area = pi * radius.pow(2);
 
-    println!(
-        "The area of a circle with radius {:.2} is {:.5}!",
-        radius, area
-    )
+    println!("The area of a circle with radius {:.2} is {:.2}!", radius, area)
 }
